67a73c3d3b473cb2c5d025f828a6cc9dec5de9e3
dvb7836 <dvb7836@gmail.com>
2021-03-26 19:20:13 +0200

    added few validators

--- love_geometry/peg_parser/consts.py
+++ love_geometry/peg_parser/consts.py
@@ -2,4 +2,3 @@ LOVE_CASE_SEPARATORS = [",", "but", "while", "and", "\n"]
 FEELINGS = ["loves", "hates"]
 SENTENCE_SEPARATOR = "."
 DOUBLE_ENDED_RELATIONSHIP_FLAG_KEYWORD = "mutually"
-ALLOW_HIGH_SELF_ESTEEM = True
--- love_geometry/server/services/validator.py
+++ love_geometry/server/services/validator.py
@@ -2,14 +2,20 @@ from __future__ import annotations
 import itertools
 
 from love_geometry.server.exceptions import ValidationError
-from pypeg2.xmlast import create_tree, xml2thing, thing2xml
-from lxml import etree
+from pypeg2.xmlast import create_tree
 from xmldiff import main
+
+from ..consts import ALLOW_FEELINGS_TO_ITSELF
 
 
 class LoveStoryValidator:
+
+    def validate(self, parsed_love_story):
+        self.check_for_duplicated_sentence(parsed_love_story)
+        self.check_for_duplicated_love_case(parsed_love_story)
+
     @staticmethod
-    def validate(parsed_love_story):
+    def check_for_duplicated_sentence(parsed_love_story):
         xml_tree = create_tree(parsed_love_story)
 
         sentences = xml_tree.findall('Sentence')
@@ -17,6 +23,14 @@ class LoveStoryValidator:
             for sentence1, sentence2 in itertools.combinations(sentences, 2):
                 if not main.diff_trees(sentence1, sentence2):
                     raise ValidationError(f"duplicated sentences")
+
+    @staticmethod
+    def check_for_duplicated_love_case(parsed_love_story):
+        for sentence in parsed_love_story:
+            for origin_name, love_case in list(sentence.items()):
+                for feeling, people in love_case.items():
+                    if origin_name in people and not ALLOW_FEELINGS_TO_ITSELF:
+                        raise ValidationError(f"feelings to itself are not permitted")
 
     @staticmethod
     def find_cheaters(sentence: dict) -> list:
